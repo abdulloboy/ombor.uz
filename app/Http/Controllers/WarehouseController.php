a4681b5b49a4f8a326d1301f5c2c73ad30812c11
abdulloboy <89206946+abdulloboy@users.noreply.github.com>
2021-09-10 16:10:12 +0500

    API documentation created with Scribe

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -8,11 +8,28 @@ use Illuminate\Http\Request;
 
 class WarehouseController extends Controller
 {
-    /**
+    /** 
      * Ombor.uzga API orqali JSON so'rov berilganda ishlaydi va natijani qaytaradi.
-     *
+     * Misol:  
+     * [
+     *  { 
+     *      "product_id": 1, 
+     *      "quantity": 30 
+     *  },
+     *  { 
+     *      "product_id": 2, 
+     *      "quantity": 2
+     *  }
+     * ]
+     * 
+     * 
+     * @bodyParam products object[] required Mahsulot ro'yhati. 
+     * @bodyParam products.product_id integer required So'ralayotgan mahsulot Id si yoziladi.
+     * @bodyParam products.quantity integer required So'ralayotgan mahsulotdan nechta kerakligi yoziladi. 
+     * @response {"result":[{"product_name":"Ko'ylak","product_qty":30,"product_materials":[{"warehouse_id":1,"material_name":"Mato","qty":12,"price":1500},{"warehouse_id":2,"material_name":"Mato","qty":12,"price":1600},{"warehouse_id":3,"material_name":"Ip","qty":40,"price":500},{"warehouse_id":4,"material_name":"Ip","qty":260,"price":550},{"warehouse_id":5,"material_name":"Tugma","qty":150,"price":300}]},{"product_name":"Shim","product_qty":20,"product_materials":[{"warehouse_id":2,"material_name":"Mato","qty":28,"price":1600},{"warehouse_id":4,"material_name":"Ip","qty":40,"price":550},{"warehouse_id":null,"material_name":"Ip","qty":260,"price":null},{"warehouse_id":6,"material_name":"Zamok","qty":20,"price":2000}]}]}
      * @return \Illuminate\Http\Response
-     */
+    */
+    
     public function index(Request $request)
     {
         $result=[];
